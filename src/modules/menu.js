52dd2e0253d8be9802f6e62240e9b81d3bfaecce
Vlad Mansurov <m_v2709@mail.ru>
2022-06-29 14:17:29 +0300

    Выполнил сложное задание

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,7 +2,9 @@ const menu = () => {
   const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a ");
+  const menuItems = menu.querySelectorAll("ul>li>a");
+  const scrollNext = document.querySelectorAll("a")[0];
+  const scrollNextBlock = document.getElementById("service-block");
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
@@ -11,7 +13,30 @@ const menu = () => {
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
 
-  menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
+  const scroll = (step) => {
+    window.scrollBy({
+      top: step,
+      behavior: "smooth"
+    });
+  };
+
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', (e) => {
+      const menuAttr = menuItem.getAttribute('href');
+      const stepMenu = document.querySelector(menuAttr).getBoundingClientRect().top;
+      
+      e.preventDefault();
+      scroll(stepMenu);
+      handleMenu();
+    });
+  });
+
+  scrollNext.addEventListener('click', (e) => {
+    const blockPosition = scrollNextBlock.getBoundingClientRect().top;
+    e.preventDefault();
+    scroll(blockPosition);
+  });
+
 };
 
 export default menu;
