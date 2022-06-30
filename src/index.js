a793293e46e59200a77812a864436e0dfaedb3a4
Vlad Mansurov <m_v2709@mail.ru>
2022-07-01 01:22:21 +0300

    Выполнил 23 задание

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import validate from "./modules/validate";
 import tabs from "./modules/tabs";
 import scrollToNext from "./modules/scrollToNext";
+import slider from "./modules/slider";
 
 timer('30 june 2022');
 menu();
@@ -11,3 +12,4 @@ modal();
 validate();
 tabs();
 scrollToNext();
+slider();
